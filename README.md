86f1b0e9217049e00232b7ea39c7429ba128a66e
Emma Clark <emmabossuytclark@gmail.com>
2025-08-13 13:49:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- Etch-a-Sketch  -->
+
+This is a browser-based Etch-a-Sketch style app where the user can control the size of the pixels. The user enters any number between 1 and 100 to set how many pixels run along one row of the canvas. Users can then draw by moving their mouse over the pixels.
+
+<!-- How It Works -->
+
+- User selects how many pixels they want
+- Pick a color
+- Mouseover event listener allows the user to start drawing on the canvas
+- Reset or erase
+
+<!-- Why I Made It -->
+
+For fun.  
+It’s a quick HTML/CSS/JS experiment to mess around with the DOM and event listeners.
+
+<!-- Tech Used -->
+
+- HTML
+- CSS
+- JavaScript
---
+++ js/canvasHandler.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById("canvas");
+function createPixels(input) {
+  let totalPixels = input ** 2;
+
+  // Seperated the height and width values of the dimentions of both the canvas
+  // and the pixels incase the grid is not perfectly square in future
+  let canvasHeight = canvas.clientHeight;
+  let canvasWidth = canvas.clientWidth;
+
+  let pixelHeight = canvasHeight / input;
+  let pixelWidth = canvasWidth / input;
+
+  // Dynamically create pixels and append to canvas
+  for (let i = 0; i < totalPixels; i++) {
+    let pixel = document.createElement("div");
+    pixel.style.height = pixelHeight + "px";
+    pixel.style.width = pixelWidth + "px";
+    pixel.classList.add("pixel"); // general styling
+    canvas.appendChild(pixel);
+  }
+}
+
+createPixels(100); //test

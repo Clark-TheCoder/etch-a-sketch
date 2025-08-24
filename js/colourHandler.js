bdb9f86863b76cf2a367a65a93963ea072ebffdb
Emma Clark <emmabossuytclark@gmail.com>
2025-08-24 18:05:25 -0300

    Removed unnecessary function. Added event to get the colour value from the colour input.

--- js/colourHandler.js
+++ js/colourHandler.js
@@ -4,10 +4,12 @@ const quickColours = document.getElementById("quick_colours");
 [...quickColours.children].forEach((child) => {
   child.addEventListener("click", (e) => {
     let value = e.target.value;
-    setColour(value);
+    return (selectedColour = value);
   });
 });
 
-function setColour(value) {
-  return (selectedColour = value);
-}
+const colourPicker = document.getElementById("colour_picker");
+
+colourPicker.addEventListener("input", (e) => {
+  return (selectedColour = e.target.value);
+});
